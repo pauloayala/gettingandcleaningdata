068fc5bb4bab55b1c555de13807ec1413269762b
pauloayala <pauloayala@gmail.com>
2019-10-20 12:44:39 -0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+## Step 1: Download the dataset
 filename <- "Coursera_DS3_Final.zip"
 
 if (!file.exists(filename)){
@@ -10,7 +10,7 @@ if (!file.exists(filename)){
 if (!file.exists("UCI HAR Dataset")) { 
   unzip(filename) 
 }
-
+## Step 2 : Assigning each data to variables
 features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -20,13 +20,16 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
+# Step 3: Joined the training and the test data sets to create one data set
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
 Joined_Data <- cbind(Subject, Y, X)
 
+# Step 4: Extracts only the measurements on the standard deviation and the mean to each measurement
 TidyData <- Joined_Data %>% select(subject, code, contains("mean"), contains("std"))
 
+# Step 5: Uses descriptive activity names to call the activities in the data set.
 names(TidyData)[2] = "activity"
 names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
 names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
@@ -41,5 +44,6 @@ names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRU
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
+# Step 6: Creates a second tidy data set with the average of each variable for each activity and each subject.
 LastData <- TidyData %>% group_by(subject, activity) %>% summarise_all(list(mean)
 write.table(LastData, "LastData.txt", row.name=FALSE)
